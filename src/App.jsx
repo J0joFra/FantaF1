d18c4cbca0646cb98df5830668eea604ac854287
Joaquim Francalanci <joaquim.francalanci@ogury.co>
2026-06-03 18:10:00 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Home from './pages/Home';
 import Calculator from './pages/Calculator';
 import Compare from './pages/Compare';
 import Ferrari from './pages/Ferrari';
-import Privacy from './components/Privacy'
 
 function App() {
   return (
